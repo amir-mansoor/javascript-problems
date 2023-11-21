97165cf90d1181be15b59d7024b5f3e7f1db9ef1
amir-mansoor <amirmansoor200@gmail.com>
2023-11-21 18:31:46 +0500

    linked list

--- linkedlist.js
+++ linkedlist.js
@@ -18,11 +18,39 @@ class LinkedList {
 
     insertFirst(data) {
         this.head = new Node(data, this.head)
+        this.size++
     }
 
     // Insert last node
+    insertLast(data) {
+        let node = new Node(data)
+        let current;
+
+        if(!this.head) {
+            this.head = node
+        } else {
+            current = this.head
+            while(current.next) {
+                current = current.next
+
+            }
+
+            current.next = node
+        }
+
+        this.size++
+    }
 
     // Insert at index
+
+    insertAt(data, index) {
+        // if index is out of range
+        if(index > 0 && index > this.size) {
+            return
+        }
+
+        
+    }
 
     // Get at index
 
@@ -48,5 +76,6 @@ const ll = new LinkedList()
 ll.insertFirst(100)
 ll.insertFirst(200)
 ll.insertFirst(300)
+ll.insertLast(400)
 ll.printListData()
 
