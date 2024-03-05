7b7b7459328c3b30192248d29d5216b8a1826c70
Amir Mansoor <amirmansoor200@gmail.com>
2024-03-05 14:55:27 +0500

    updated example of linkedlist

--- linkedlist.js
+++ linkedlist.js
@@ -1,81 +1,18 @@
 class Node {
-    constructor(data, next = null) {
-        this.data = data
-        this.next = next
-    }
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
-
-const n1 = new Node(100)
-
 
 class LinkedList {
-    constructor() {
-        this.head = null
-        this.size = 0
-    }
+  constructor() {
+    this.head = null;
+  }
 
-    // Insert first node
-
-    insertFirst(data) {
-        this.head = new Node(data, this.head)
-        this.size++
-    }
-
-    // Insert last node
-    insertLast(data) {
-        let node = new Node(data)
-        let current;
-
-        if(!this.head) {
-            this.head = node
-        } else {
-            current = this.head
-            while(current.next) {
-                current = current.next
-
-            }
-
-            current.next = node
-        }
-
-        this.size++
-    }
-
-    // Insert at index
-
-    insertAt(data, index) {
-        // if index is out of range
-        if(index > 0 && index > this.size) {
-            return
-        }
-
-        
-    }
-
-    // Get at index
-
-    // Remove at index
-
-    // Clear List
-
-    // Print list data
-
-    printListData() {
-        let current = this.head
-
-        while(current) {
-            console.log(current.data)
-            current = current.next
-        }
-    }
-
+  addFirst(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
-
-const ll = new LinkedList()
-
-ll.insertFirst(100)
-ll.insertFirst(200)
-ll.insertFirst(300)
-ll.insertLast(400)
-ll.printListData()
-
